094b5f6573ced40cd1959a07a2f855eddb911e64
sakura-lolipop <55526806+sakura-lolipop@users.noreply.github.com>
2026-09-13 20:52:09 +0800

    feat: v2.2 (late-load.sh)

--- late-load.sh
+++ late-load.sh
@@ -8,10 +8,10 @@ done
 [ -z "$RP" ] && RP=$(command -v resetprop 2>/dev/null)
 if [ -z "$RP" ] || [ ! -f "$CONF" ]; then exit 0; fi
 . "$CONF"
-[ -n "$OS_INC" ] && "$RP" ro.mi.os.version.incremental "$OS_INC" 2>/dev/null
-[ -n "$OS_NAME" ] && "$RP" ro.mi.os.version.name "$OS_NAME" 2>/dev/null
-[ -n "$OS_CODE" ] && "$RP" ro.mi.os.version.code "$OS_CODE" 2>/dev/null
-[ -n "$BUILD_INC" ] && "$RP" ro.build.version.incremental "$BUILD_INC" 2>/dev/null
-[ -n "$SYS_INC" ] && "$RP" ro.system.build.version.incremental "$SYS_INC" 2>/dev/null
-[ -n "$XMS_INC" ] && "$RP" ro.mi.xms.version.incremental "$XMS_INC" 2>/dev/null
+[ -n "$OS_INC" ] && "$RP" -n ro.mi.os.version.incremental "$OS_INC" 2>/dev/null
+[ -n "$OS_NAME" ] && "$RP" -n ro.mi.os.version.name "$OS_NAME" 2>/dev/null
+[ -n "$OS_CODE" ] && "$RP" -n ro.mi.os.version.code "$OS_CODE" 2>/dev/null
+[ -n "$BUILD_INC" ] && "$RP" -n ro.build.version.incremental "$BUILD_INC" 2>/dev/null
+[ -n "$SYS_INC" ] && "$RP" -n ro.system.build.version.incremental "$SYS_INC" 2>/dev/null
+[ -n "$XMS_INC" ] && "$RP" -n ro.mi.xms.version.incremental "$XMS_INC" 2>/dev/null
 exit 0
