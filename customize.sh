c517384b75a74f8cd824c1ed504a4c085aac7ff0
sakura-lolipop <55526806+sakura-lolipop@users.noreply.github.com>
2026-09-13 20:52:17 +0800

    feat: v2.2 (customize.sh)

--- customize.sh
+++ customize.sh
@@ -1,4 +1,5 @@
-ui_print "- XiaoMi-UPDATE-Disable v2.0"
+ui_print "- XiaoMi-UPDATE-Disable v2.1 完全版"
+ui_print "- 支持: 常规 Magisk/KSU 与 late-load 模式"
 CONF=/data/adb/love_spoof.conf
 if [ ! -f "$CONF" ]; then
   cat > "$CONF" <<EOC
@@ -11,8 +12,8 @@ XMS_INC=🌸
 ORIG_OS_INC=$(getprop ro.mi.os.version.incremental)
 ORIG_XMS_INC=$(getprop ro.mi.xms.version.incremental)
 EOC
-  ui_print "- 初始配置: Love.🌸 (WebUI 里可改)"
+  ui_print "- 初始配置: Love.🌸 (WebUI 可改)"
 else
-  ui_print "- 已有配置, 保留不动"
+  ui_print "- 保留已有配置"
 fi
 set_perm_recursive "$MODPATH" 0 0 0755 0644
